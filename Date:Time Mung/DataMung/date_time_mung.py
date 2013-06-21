afdae64b89a2d2784a4e0682f3a5df31bed1f3cc
Bill <sheltowt@gmail.com>
2013-06-21 14:30:47 -0700

    developed ML dataset including date/time features

--- Date:Time Mung/DataMung/date_time_mung.py
+++ Date:Time Mung/DataMung/date_time_mung.py
@@ -4,11 +4,19 @@ import dateutil.parser
 
 unmunged = pd.read_csv("/Users/williamshelton/Desktop/PIXLEE_predictive_modeling/Date:Time Mung/Datasets/time_hashtag_dataset.csv")
 
+unmunged["day_week"] = 0
+
+unmunged["hour_day"] = 0
+
+count = -1
 for x in unmunged["created_at"]:
+  count += 1
   try:
     x = dateutil.parser.parse(x)
     z = datetime.date.timetuple(x)
-    print z[6]
-    print x.hour
+    unmunged["day_week"][count] = z[6]
+    unmunged["hour_day"][count] = x.hour
   except:
     pass
+
+unmunged.to_csv("/Users/williamshelton/Desktop/PIXLEE_predictive_modeling/Date:Time Mung/Datasets/time_hashtag_MLdataset.csv")
